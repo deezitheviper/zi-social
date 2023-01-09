c2379ebf0c235aa7cd88d4907c6edebf0f87e4b6
deezitheviper <amuhdavid@gmail.com>
2023-01-09 14:40:20 +0100

    update on auth styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,61 @@
 
 import {
   createBrowserRouter,
+  Navigate,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
+import Leftbar from "./components/Leftbar";
+import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
 
+
+
+
+
+function App() {
+
+  const currentUser = false;
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar/>
+        <div>
+          <Leftbar/>
+          <Outlet/>
+          <Rightbar/>
+        </div>
+      </div>
+    )
+}
+
+  const ProtectedRoute = ({children}) => {
+      if(!currentUser) {
+        return <Navigate to="/Login" />
+      }
+      return children
+  }
 
 const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <ProtectedRoute><Layout/></ProtectedRoute>,
+    children: [
+      {
+        path: "/Home",
+        element: <Home/>
+      },
+      {
+        path: "/Profile/:id",
+        element: <Profile/>
+      }
+    ]
+  },
   {
     path: "/Login", 
     element: <Login/> 
@@ -18,7 +66,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-function App() {
   return (
     <div>
     <RouterProvider router={router} />
