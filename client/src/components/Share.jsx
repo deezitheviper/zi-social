cd72d54b5e78deab2d0ac570ff5cfabd27d045df
deezitheviper <amuhdavid@gmail.com>
2023-01-12 22:42:42 +0100

    share post

---
+++ client/src/components/Share.jsx
@@ -0,0 +1,53 @@
+
+import "./styles/Share.scss";
+import {BiImages} from "react-icons/bi";
+import { useContext } from "react";
+import {CiMapPin} from 'react-icons/ci';
+import {FaUserFriends} from 'react-icons/fa';
+import avatar from '../assets/img/avatar.png';
+import { AuthContext } from "../context/AuthContext";
+
+
+const Share = () => {
+    
+  //const {currentUser} = useContext(AuthContext)
+  return (
+    <div className="share">
+      <div className="container">
+        <div className="top">
+          <img
+            src={avatar}
+            alt=""
+          />
+          <input type="text" placeholder={`What's on your mind Deezi?`} />
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <input type="file" id="file" style={{display:"none"}} />
+            <label htmlFor="file">
+              <div className="item">
+               <BiImages/>
+                <span>Add Image</span>
+              </div>
+            </label>
+            <div className="item">
+              <CiMapPin />
+              <span>Add Place</span>
+            </div>
+            <div className="item">
+        
+              <FaUserFriends/>
+              <span>Tag Friends</span>
+            </div>
+          </div>
+          <div className="right">
+            <button>Share</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
