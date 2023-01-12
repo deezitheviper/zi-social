aadff9d58618f503e19a01e1b12eedf41490c623
deezitheviper <amuhdavid@gmail.com>
2023-01-12 22:45:20 +0100

    update on pages and shii

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Posts from '../../components/Posts';
 import Reels from '../../components/Reels';
+import Share from '../../components/Share';
 import './Home.scss';
 
 const Home = () => {
   return (
     <div className='home'>
      <Reels/>
+     <Share/>
      <Posts/>
     </div>
   )
