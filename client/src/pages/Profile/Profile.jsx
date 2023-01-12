fa29424a3cb8ece2d6daac926c539375abdc310e
deezitheviper <amuhdavid@gmail.com>
2023-01-12 17:03:16 +0100

    profile p done

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,20 +1,27 @@
 import './Profile.scss';
 import {CiMail} from 'react-icons/ci';
 import avatar from "../../assets/img/avatar.png";
+import Posts from '../../components/Posts';
+
+
 
 const Profile = () => {
   return (
     <div className='profile'>
-      
-        <img src={avatar} alt="" className="avatar" />
-        <div className="container">
-            <div className="userInfo">
+      <div className="header">
+      <img src='https://images.unsplash.com/photo-1626276727721-e5fb4a008389?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjI3fHxzZXR1cHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60' className='cover'/>
+        <img src={avatar} alt="" className="pavatar" /> 
+      </div> 
+        <div className="pContainer">
+            <div className="pInfo">
             <span>Deezi TheViper</span>
-                <div className="details">
+                <div className="pDetails">
                     <CiMail/>
                 </div>
 
-            </div>
+            </div> 
+
+            <Posts/>
         </div>
     </div>
   )
