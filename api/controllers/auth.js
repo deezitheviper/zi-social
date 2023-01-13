5376bed7663a96f391ced944c7c074e801255378
deezitheviper <amuhdavid@gmail.com>
2023-01-13 17:35:26 +0100

    login and reg shii don

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import {db} from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
     const {username,email,name, password} = req.body;
@@ -21,4 +22,28 @@ export const register = (req, res) => {
         })
     })
 
+}
+
+export const login = (req, res) => {
+    const {username} = req.body;
+
+    const q = "SELECT * FROM users WHERE username = ?"
+    db.query(q,[username], (err, data) => {
+        if(err) return res.status(500).json(err);
+        if(data.length === 0) return res.status(404).json("User not found");
+        const userpassword = data[0].password
+        const checkPassword = bcrypt.compareSync(req.body.password, userpassword)
+        if(!checkPassword) return res.status(404).json("Wrong Credentials")
+
+        const token = jwt.sign({
+            id:data[0].id},
+            "secretkey"
+        )
+        
+        const {password, ...others} = data[0]
+
+        res.cookie("accessToken", token, {
+            httpOnly:true,
+        }).status(200).json(others)
+    })
 }
