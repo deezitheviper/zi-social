893db583c634128db60eb2b5e1c563fa06ea1319
deezitheviper <amuhdavid@gmail.com>
2023-01-14 14:37:31 +0100

    add post api working successfully

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,6 +1,6 @@
 import { db } from "../db.js";
 import jwt from "jsonwebtoken";
-
+import moment from "moment";
 
 export const getPosts = (req, res, next) => {
 
@@ -17,3 +17,24 @@ export const getPosts = (req, res, next) => {
     })
  
 } 
+
+export const addPost = (req, res) => {
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(403).json("Not authorized");
+    jwt.verify(token, "secretkey", (err, userData) => {
+
+        if(err) return res.status(403).json("Invalid Token")
+        const {content,img} = req.body;
+        const q = "INSERT INTO posts (`content`, `img`, `createdOn`, `user`) VALUES (?)";
+        const values = [
+            content,
+            img,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userData.id
+        ]
+        db.query(q,[values], (err, data) => {
+            if(err) return res.status(500).json(err);
+            return res.status(200).json("Post created successfully");
+        })
+    })
+}
